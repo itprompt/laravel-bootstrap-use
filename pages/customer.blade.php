3f9c99259b65d41b02348d2a97916bee9a481675
itprompt <itpromptdev@gmail.com>
2021-07-31 03:07:05 +0700

    update pages

--- pages/customer.blade.php
+++ pages/customer.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.screen')
 @section('content')
 <!-- Content Area -->
-<div class="container">
-    <h1 class="text-primary">Customer ลูกค้า/สมาชิก</h1>
+<div class="container bg-warning p-4 my-4" style="height: 500px;">
+    <h1 class="text-white">Customer ลูกค้า/สมาชิก</h1>
 </div>
+<!-- End Content -->
 @endsection
--- pages/main.blade.php
+++ pages/main.blade.php
@@ -1,38 +1,8 @@
 @extends('layouts.screen')
 @section('content')
-<div class="container py-5">
-    <h1>ระบบงาน IT System</h1>
-    <div class="card-group">
-        <div class="card bg-primary">
-            <div class="card-body">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Text</p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body bg-warning">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Text</p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body bg-danger">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Text</p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body bg-success">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Text</p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body bg-secondary">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Text</p>
-            </div>
-        </div>
-    </div>
+<!-- Content Area -->
+<div class="container bg-dark p-4 my-4" style="height: 500px;">
+    <h1 class="text-white">Main Page</h1>
 </div>
+<!-- End Content -->
 @endsection
